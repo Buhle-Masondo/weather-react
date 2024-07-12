7f345f273b5dbd4f5e58bd091282e42bb96e7553
Buhle-Masondo <buhlemm22@gmail.com>
2024-07-12 23:50:35 +0200

    updated index,js and  imported WeatherInfo,js in Weather.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React from "react";
 import "./App.css";
 import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather App</h1>
-        <WeatherSearch />
-      </header>
+      <div className="container">
+        <Weather defaultCity="New York" />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/Buhle-Masondo"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Buhle Masondo
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Buhle-Masondo/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
