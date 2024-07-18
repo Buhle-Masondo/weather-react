704851f8e18767b925be3ec0ef95fbf1e71a51ca
Buhle-Masondo <buhlemm22@gmail.com>
2024-07-18 18:05:52 +0200

    added WeatherForecast.js and WeatherForecast.css

---
+++ src/WeatherForcast.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function weatherForecast() {
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        <div className="col">
+          <div className="WeatherForecast-day">Thu</div>
+          <Weathericon code="01d" />
+          <div className="WeatherForecast-temperature">
+            <span className="WeatherForecast-temperature-max">19</span>
+            <span className="WeatherForecast-temperature-min">10</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
