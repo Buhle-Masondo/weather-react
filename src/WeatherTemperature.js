a55b43595ad119e46c0a69e0e96d1454fb6ddaac
Buhle-Masondo <buhlemm22@gmail.com>
2024-07-21 23:40:05 +0200

    added final touches

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,10 +1,14 @@
 import React from "react";
+import "./Weather.css";
 
 export default function WeatherTemperature(props) {
   return (
     <div className="WeatherTemperature">
+      {" "}
       <span className="temperature">{Math.round(props.celsius)}</span>
-      <span className="unit">°C</span>
+      <span className="unit">
+        <strong> °C</strong>
+      </span>
     </div>
   );
 }
